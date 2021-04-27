80d4348e9be71f8cb978ee68ff4b2e39c1f8cb46
BrianBell422 <brianbell.uk@gmail.com>
2021-04-27 15:33:35 +0100

    console added

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,12 +1,16 @@
+import os
 import psycopg2
 import psycopg2.extras as ext
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 def run_sql(sql, values = None):
     conn = None
     results = []
 
     try:
-        conn=psycopg2.connect("dbname='mikes_bikes'")
+        conn=psycopg2.connect(DATABASE_URL, sslmode='require')
+        # conn=psycopg2.connect("dbname='mikes_bikes'")
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
